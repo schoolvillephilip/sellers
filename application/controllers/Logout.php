c20d132bca53d74509bbbe63034485a8ed5a692f
Sokoya Philip <sokoyaphilip@yahoo.com>
2018-12-04 09:54:12 +0100

    Logout

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -6,13 +6,7 @@ class Logout extends CI_Controller
 
     public function index()
     {
-        $sessions = $this->session->all_userdata();
-        foreach ($sessions as $key => $value) {
-            if ($key != 'cart_contents') {
-                $this->session->unset_userdata($key);
-            }
-        }
-        $this->session_destroy();
+        $this->session->sess_destroy();
         redirect(base_url('login'));
     }
 
--- application/controllers/Reset.php
+++ application/controllers/Reset.php
@@ -31,83 +31,15 @@ class Reset extends CI_Controller
     {
 
         $this->form_validation->set_rules('email', 'Email Address', 'trim|required|xss_clean|valid_email');
-        // $this->form_validation->set_message('is_unique', 'The %s is already taken');
-        $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean|min_length[8]|max_length[15]');
-        $this->form_validation->set_rules('confirm_password', 'Password', 'trim|required|xss_clean|min_length[8]|max_length[15]|matches[password]');
         if ($this->form_validation->run() === FALSE) {
-            $this->session->set_flashdata('error_msg', '<strong>There was an error when creating the account. Please fix the following</strong> <br />' . validation_errors());
-            redirect('register');
+            $this->session->set_flashdata('error_msg', '<strong>Please the error</strong> <br />' . validation_errors());
+            redirect('reset');
         } else {
             // Check if email address is in the system
             $email = $this->input->post('email');
-            $user = $this->seller->get_row('users', '*', "( email = '$email' ) ");
-            $license_to_sell = ($this->input->post('license_to_sell') == true) ? 1 : 0;
-            $plat = $this->input->post('platform');
-            $platform = ( !empty( $plat )) ? $plat : '';
-            $seller_data = array(
-                'legal_company_name' => $this->input->post('legal_company_name'),
-                'address' => $this->input->post('store_address'),
-                'tin' => $this->input->post('tin'),
-                'main_category' => $this->input->post('main_category'),
-                'license_to_sell' => $license_to_sell,
-                'reg_no' => $this->input->post('reg_no'),
-                'no_of_products' => $this->input->post('no_of_products'),
-                'product_condition' => $this->input->post('product_condition'),
-                'platform_selling' => $platform,
-                'account_number' => $this->input->post('account_number'),
-                'account_name' => $this->input->post('account_name'),
-                'bank_name' => $this->input->post('bank_name'),
-                'account_type' => $this->input->post('account_type'),
-                'date_applied' => get_now()
-            );
-            $user_data = array(
-                'is_seller' => 'pending',
-                'first_name' => $this->input->post('first_name'),
-                'last_name' => $this->input->post('last_name'),
-                'phone' => $this->input->post('phone_number')
-            );
-            if ($user) {
-                // we are updating
-                $seller_data['uid'] = $user->id;
-                // Do we already have this user in seller table
-                if( $this->seller->get_row('sellers','id', "( uid = $user->id ) ") ){
-                    $this->session->set_flashdata('error_msg', 'You already have a seller account. login with your details to access your dashboard.');
-                    redirect('login');
-                }
-                if ($this->seller->create_account($seller_data, 'sellers')) {
-                    $this->seller->update_data($user->id, $user_data, 'users');
-                    $this->session->set_flashdata('success_msg', 'Congrats your application has been received and under review, you will be mailed on the update.');
-                    // Email Model
-                    redirect('login');
-                } else {
-                    $this->session->set_flashdata('error_msg', 'There was an error creating your seller account, please you can contact support if it persist.');
-                    redirect('register/form');
-                }
-            } else {
-                // we are creating
-                $user_data['email'] = $this->input->post('email');
-                $salt = salt(50);
-                $user_data['password'] = shaPassword($this->input->post('password'), $salt);
-                $user_data['salt'] = $salt;
-                $user_data['date_registered'] = get_now();
-                $user_data['last_login'] = get_now();
-                $user_data['is_seller'] = 'pending';
-                $user_id = $this->seller->create_account($user_data, 'users');
-                if (!is_numeric($user_id)) {
-                    $this->session->set_flashdata('error_msg', 'There was an error creating your account, please you can contact support if it persist.');
-                    redirect('register/form');
-                } else {
-                    $seller_data['uid'] = $user_id;
-                    if ($this->seller->create_account($seller_data, 'sellers')) {
-                        // Email Model
-                        $this->session->set_flashdata('success_msg', 'Congrats your application has been received and under review, you will be mailed on the update.');
-                        redirect('login');
-                    } else {
-                        $this->session->set_flashdata('error_msg', 'There was an error creating your seller account, please you can contact support if it persist.');
-                        redirect('register/form');
-                    }
-                }
-            }
+            $user = $this->seller->get_row('users', 'id', "( email = '$email' ) ");
+
+
         }
     }
 }
