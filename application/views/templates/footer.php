7b38e9fb44b15c730f238d8796f6756dc2504535
Adedoyin Skilz <michaeladekoniye@gmail.com>
2019-03-15 14:18:18 +0100

    //Change Log

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,10 @@
 <footer id="footer">
     <p class="pad-lft">© 2015 - <?= date('Y'); ?> Internet Onitshamarketing Limited All rights reserved</p>
 </footer>
+<script>
+    var HW_config = {
+        selector: ".change_log_list", // CSS selector where to inject the badge
+        account: "yoN2XJ" // your account ID, you can find it in Settings->Widget
+    };
+</script>
+<script async src="https://cdn.headwayapp.co/widget.js"></script>
